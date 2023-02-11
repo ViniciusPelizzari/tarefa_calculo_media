952bc556e9d65b13a85f6d5a2f936969f79293cc
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-11 08:20:37 -0300

    Versão 1.2 - Código comentado

--- Calculo.java
+++ Calculo.java
@@ -1,9 +1,11 @@
-public class Calculo{
+public class Calculo {
 
+    // declaração das notas
     int nota1 = 60;
     int nota2 = 89;
     float nota3 = 65.8f;
     int nota4 = 100;
-    double media = (nota1+nota2+nota3+nota4)/4;
-    
+    // soma e divisão
+    double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
 }
--- Media.java
+++ Media.java
@@ -3,9 +3,12 @@ import javax.swing.JOptionPane;
 public class Media {
 
     public static void main(String[] args) {
+        // criação de objeto
         Calculo calculo = new Calculo();
+        // apresentação no terminal
         System.out.println(calculo.media);
 
+        // mensagem na tela
         JOptionPane.showMessageDialog(null, calculo.media, "Resultado Média 4 notas", JOptionPane.WARNING_MESSAGE);
     }
 
